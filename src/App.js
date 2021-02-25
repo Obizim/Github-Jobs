5496c64f382577f446e59395f2fbd60be4935304
Zim <obizimuzo@gmail.com>
2021-02-25 13:48:07 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <Container>Hello. Github Jobs</Container>
+    </div>
+  );
+}
+
+export default App;
